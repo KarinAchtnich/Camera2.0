3fe6cf72927f7f2d17a4dbc352d958d0d261bbaa
KarinAchtnich <39257985+KarinAchtnich@users.noreply.github.com>
2018-10-02 11:20:06 +0200

    Create own.js

---
+++ js/own.js
@@ -0,0 +1,11 @@
+// Add Firebase
+var firebase = require("Firbase");
+var config = {
+    apiKey: "AIzaSyCQCKqVSKAeOgvDz-zQNiLQEhQCY1YDhbM",
+    authDomain: "camera2-21e00.firebaseapp.com",
+    databaseURL: "https://camera2-21e00.firebaseio.com",
+    storageBucket: "gs://camera2-21e00.appspot.com",
+  };
+  firebase.initializeApp(config);
+  
+  // Use Firebase
